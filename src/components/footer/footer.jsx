a02ba13435965b08079112b714d1516aee566b90
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-21 18:26:27 +1030

    Add login components

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = (props) => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Welcome to Business Card Maker</p>
+  </footer>
+);
+
+export default Footer;
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import firebase from 'firebase';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -7,4 +7,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
