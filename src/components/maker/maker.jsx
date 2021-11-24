a7f19a301eb91a4b85f1756f8f8553e35c9834ed
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-24 21:04:05 +1030

    Add editor&preview layout

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -2,10 +2,13 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
   const navigate = useNavigate();
+
   const onLogout = () => {
     authService.logout();
   };
@@ -21,6 +24,10 @@ const Maker = ({ authService }) => {
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
