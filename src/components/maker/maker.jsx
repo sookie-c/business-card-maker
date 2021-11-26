5b4d324052b381c819d607a9538afbed72fe0131
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-26 13:20:05 +1030

    Handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: '1',
       name: 'Sookie',
       company: 'SK telecom',
@@ -19,7 +19,7 @@ const Maker = ({ authService }) => {
       fileName: 'sookie',
       fileURL: null,
     },
-    {
+    2: {
       id: '2',
       name: 'Bob',
       company: 'Samsung',
@@ -30,7 +30,7 @@ const Maker = ({ authService }) => {
       fileName: 'bob',
       fileURL: 'bob.png',
     },
-    {
+    3: {
       id: '3',
       name: 'Kim',
       company: 'Google',
@@ -41,7 +41,7 @@ const Maker = ({ authService }) => {
       fileName: 'kim',
       fileURL: null,
     },
-  ]);
+  });
 
   const navigate = useNavigate();
   const onLogout = () => {
@@ -56,18 +56,33 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    console.log('here');
+  const createOrUpdateCard = (card) => {
     console.log(card);
-    const updated = [...cards, card];
-    setCards(updated);
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
   };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
